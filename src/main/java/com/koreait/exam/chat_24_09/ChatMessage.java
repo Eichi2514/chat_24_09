b2d7340b7330805c3d3d7c68c879f783fe8d0b73
Eichi2514 <picon1317@gmail.com>
2024-09-05 16:46:54 +0900

    RsData 타입 생성

---
+++ src/main/java/com/koreait/exam/chat_24_09/ChatMessage.java
@@ -0,0 +1,22 @@
+package com.koreait.exam.chat_24_09;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ChatMessage {
+    private long id;
+    private LocalDateTime createDate;
+    private String authorName;
+    private String content;
+
+    public ChatMessage(String authorName, String content) {
+        this(1, LocalDateTime.now(), authorName, content);
+    }
+
+}
